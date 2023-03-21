927b6d1a984679ddabc0e281651236343ef61421
Sebastián Sala <sebastiansala.dev@gmail.com>
2023-03-21 04:18:59 -0300

    started porting portfolio with custom bootstrap

---
+++ front-end/src/assets/js/main.ts
@@ -0,0 +1,9 @@
+import "bootstrap";
+
+$(function() {
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggler').css('display') !='none'){
+            $('.navbar-toggler').trigger( "click" );
+        }
+    });
+});
