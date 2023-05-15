8876290bda8a1d08064a507516861b5b5e5a7c32
Sebastián Sala <sebastiansala.dev@gmail.com>
2023-05-15 19:38:49 -0300

    Added server HealthCheck

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HealthCheckService } from './services/health-check.service';
+import { ResponseMessage } from './model/dataTypes';
 
 
 @Component({
@@ -6,10 +9,47 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 
-  constructor() {
+  constructor(private healthCheck: HealthCheckService) {
+  }
+
+
+  ngOnInit(): void {
+
+    // Checking if the backend server is running the application
+    // regardles of the database server state, only quering a message that does not connect to the database
+    this.doChecks();
+
+  }
+
+
+  private doChecks() {
+
+
+    this.healthCheck.check().subscribe({
+
+      next: (response) => {
+        const message = response as unknown as ResponseMessage
+        console.log("-------- Status Check for Backend application");
+        console.log("-------- (only backend app, without database server)");
+        console.log("-------- 1.", message.message);
+      },
+
+      error: (err) => {
+
+        console.log("-------- Status Check, error: Backend connection fail: ", err.error.error);
+        console.log("-------- Full error message: ", err);
+
+      },
+
+      complete: () => {
+        console.log("-------- Status Check, complete: Backend connection success!");
+      }
+
+    });
+
 
   }
 
